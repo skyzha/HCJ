66e7acb2d8294b4d6f72d0f88650dd93c7b5d570
skyzha <rezapermana.aqi@gmail.com>
2019-09-17 08:00:45 +0700

    reading input data

---
+++ budgety/app.js
@@ -0,0 +1,48 @@
+var budgetController = (function() {
+
+})();
+
+// UI Controller
+var UIController = (function(){
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //Will be either in or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var DOM = UICtrl.getDOMstrings();
+
+    var ctrlAddItem = function() {
+        
+        // Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+        console.log(UICtrl);
+    }
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    })
+})(budgetController, UIController);
